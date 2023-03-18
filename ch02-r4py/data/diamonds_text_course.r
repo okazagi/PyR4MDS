b166ad8516f843c00b1724df21946a0a81be72f9
okazagi <63522680+okazagi@users.noreply.github.com>
2023-03-18 17:47:14 -0600

    Add files via upload

---
+++ ch02-r4py/data/diamonds_text_course.r
@@ -0,0 +1,6 @@
+diamonds <- read.csv('/Users/Elise_AGCI/Documents/GitHub/PyR4MDS/ch02-r4py/data/diamonds.csv')
+install.packages("tidyverse")
+library(readr)
+diamonds_2 <- read_csv("ch02-r4py/data/diamonds.csv")
+spec(diamonds_2)
+diamonds_2
